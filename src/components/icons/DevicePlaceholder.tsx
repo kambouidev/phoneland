a84031ea8ce2ea63ffd41abaa3c1224d5146945d
kambouidev <mohamed@squaads.com>
2025-02-18 23:53:18 +0000

    ✨ feat(icons): add DevicePlaceholder component for device representation

---
+++ src/components/icons/DevicePlaceholder.tsx
@@ -0,0 +1,11 @@
+export const DevicePlaceholder = () => (
+    <svg
+        className="w-48 h-48 text-gray-200"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        fill="currentColor"
+        viewBox="0 0 640 512"
+    >
+        <path d="M128 256c0-35.3 28.7-64 64-64h256c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H192c-35.3 0-64-28.7-64-64V256zM0 176c0-35.3 28.7-64 64-64h512c35.3 0 64 28.7 64 64v192c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V176z" />
+    </svg>
+);
