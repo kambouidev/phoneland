0e084bbd6f2035fd06769bf90a86515dc12a2865
kambouidev <mohamed@squaads.com>
2025-02-18 22:52:58 +0000

    ✨ feat(search-bar): add SearchBar component for smartphone search functionality

---
+++ src/screens/home/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent } from 'react';
+
+interface SearchBarProps {
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    onClear: () => void;
+}
+
+export const SearchBar = ({ value, onChange, onClear }: SearchBarProps) => (
+    <div className="w-full mb-4 relative flex-none">
+        <input
+            type="text"
+            placeholder="Search for a smartphone..."
+            value={value}
+            onChange={onChange}
+            className="w-full transition-all bg-foreground py-2 border-b border-black focus:outline-none pr-10"
+        />
+        {value && (
+            <button
+                onClick={onClear}
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                </svg>
+            </button>
+        )}
+    </div>
+);
