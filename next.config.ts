38733dc04dceaa7d21188aa5d122ef5f20cbe9c6
kambouidev <mohamed@squaads.com>
2025-02-18 23:53:10 +0000

    ✨ feat(config): add dynamic hostname retrieval for remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const getHostnameFromEnv = () => {
+  try {
+    const url = new URL(process.env.NEXT_PUBLIC_BASE_URL || '');
+    return url.hostname;
+  } catch {
+    return '';
+  }
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: getHostnameFromEnv(),
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
